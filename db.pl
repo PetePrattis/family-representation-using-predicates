d40a4cb0d6aa9b77159ce1969d92e4c747a11a09
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-11-13 14:21:59 +0200

    Update db.pl

--- db.pl
+++ db.pl
@@ -1,25 +1,18 @@
-/**----------------------------------------------------------------------------------------
-1η προαιρετική εργασία 
-Να αναπαρασταθεί η παραπάνω οικογένεια καθώς και 3-4 οικογένειες ακόμη (δική σας επιλογής) 
-χρησιμοποιώντας το κατηγόρημα family-3 και κατάλληλους σύνθετους όρους. 
-Ορίστε τα κατηγορήματα exists-1, dateofbirth-2 και salary-2 και χρησιμοποιήστε τα για να ανακτήσετε 
-όλους τους γονείς που γεννήθηκαν πριν το 1950 και ο μισθός τους είναι μικρότερος από 8000. 
------------------------------------------------------------------------------------------*/
+% Παναγιώτης Πράττης / Panagiotis Prattis
 
-% Παναγιώτης Πράττης Π15120
-
-/* ορίζω τα κατηγορήματα family για τις 4 οικογένεις με 3 arguments
-το 1ο argument έχει τον πατέρα, το 2ο την μητέρα και το 3ό μια λίστα με τα παιδιά
-για την εργασία μου δεν έχει χρησμότητα αυτό το predicate
- */
+/* 
+set family predicates for 4 families with 3 arguments 1st argument has father, 
+2nd argument and 3rd list of kids for my work has no use for this predicate
+*/
+ 
 family(person(tom_fox), person(ann_fox), person([pat_fox, jim_fox])).
 family(person(thomas_wayne), person(martha_wayne), person([bruce_wayne])).
 family(person(theo_pratt), person(jenny_pratt), person([pete_pratt, stevi_pratt, deppy_pratt])).
 family(person(robert_kardashian), person(kris_kardashian), person([kourtney_kardashian, kim_kardashian, khloe_kardashian])).
 
-/* ορίζω τα κατηγορήματα dateofbirth με 2 arguments 
-το 1ο argument είναι το ονοματεπώνυμο του μέλους της οικογένεις και το 2ο η πλήρης ημερομηνία γέννησης
-με αυτό το predicate θα βρω τους υποψήφιους γονείς οι οποίοι είναι γεννημένοι πριν το 1950
+/* 
+set arguments dateofbirth with 2 arguments 1st argument is the name of the family member and 2nd
+full date of birth with this predicate i will find candidate parents who were born before 1950
 */
 dateofbirth(tom_fox,date(7,may,1950)).
 dateofbirth(ann_fox,date(9,may,1951)).
@@ -42,9 +35,10 @@ dateofbirth(kourtney_kardashian,date(1,january,1975)).
 dateofbirth(kim_kardashian,date(2,february,1980)).
 dateofbirth(khloe_kardashian,date(3,march,1985)).
 
-/* ορίζω τα κατηγορήματα salary με 2 arguments 
-το 1ο argument είναι το ονοματεπώνυμο του μέλους της οικογένεις και το 2ο περιέχει την εργασία και τον μισθό
-με αυτό το predicate θα βρω τους υποψήφιους γονείς οι οποίοι είναι εργαζόμενοι με μισθό μικρότερο απο 8000
+/* 
+set arguments salary with 2 arguments 1st argument is the family member's name 
+and 2nd contains job and salary with this predicate i will find prospective parents 
+who are working with salary less than 8000
 */
 salary(tom_fox, works(bbc, 15200)).
 salary(ann_fox, works(unemployed, 0)).
@@ -67,9 +61,9 @@ salary(kourtney_kardashian, works(unemployed, 0)).
 salary(kim_kardashian, works(unemployed, 0)).
 salary(khloe_kardashian, works(unemployed, 0)).
 
-/* ορίζω τα κατηγορήματα parent με 2 arguments
-το 1ο argument είναι το ονοματεπώνυμο του γονέα της οικογένεις και το 2ο περιέχει τα ονοματεπώνυμα των παιδιών
-με αυτό το predicate θα βρω ποιά από τα persons είναι γονείς
+/* 
+i define parent predicates with 2 arguments the 1st argument is the name of the parent of the family 
+and the 2nd contains the names of the children with this predicate i will find which of the persons are parents
 */
 parent(tom_fox,(pat_fox, jim_fox)).
 parent(ann_fox,(pat_fox, jim_fox)).
@@ -83,27 +77,19 @@ parent(jenny_pratt,(pete_pratt, stevi_pratt, deppy_pratt)).
 parent(robert_kardashian,(kourtney_kardashian, kim_kardashian, khloe_kardashian)).
 parent(kris_kardashian,(kourtney_kardashian, kim_kardashian, khloe_kardashian)).
 
-/* τυπώνω οδηγίες με την εκτέλεση*/
+/* print instructions with execution*/
 :- format('recover all parents born before 1950 and their salary is less than 8,000 ~n'),
  format('type "exists(X)." to get the answer ~n').
 
-exists(X) :- /*το κατηγόρημα exists θα είναι*/
- parent(X , _), dateofbirth(X, date(_, _, T)), /*ένας γονέας, του οποίου η χρονολογία γέννησης*/
- T =< 1950,	/*είναι μικρότερη απο το 1950*/
- salary(X, works(_, M)), /*και ο μισθός του*/
- M \= 0, M =< 8000,	/*δεν είναι 0 (δηλαδή άνεργος) και έιναι μικρότερος ή ίσος με 8000*/
- format('~w born in ~w has salary of ~3f euros ~n', [X, T, M]).	/*και τελικά τυπώνω με format το αποτέλεσμα*/
+exists(X) :- /*the predicate exists will be*/
+ parent(X , _), dateofbirth(X, date(_, _, T)), /*a parent whose birth date*/
+ T =< 1950,	/*is smaller than 1950*/
+ salary(X, works(_, M)), /*and his salary*/
+ M \= 0, M =< 8000,	/*is not 0 (unemployed) and is less than or equal to 8000*/
+ format('~w born in ~w has salary of ~3f euros ~n', [X, T, M]).	/*and finally I format the result*/
  
-candidates(X) :- /* αυτό το κατηγόρημα εμφανίζει όλους τους γονείς ημερομηνία γέννησης και τον μισθό τους */
+candidates(X) :- /* this predicate shows all parents their date of birth and their salary */
  parent(X, _), dateofbirth(X, date(_, _, T)), salary(X, works(_, M)),
  format('~w born in ~w is a possible candidate for this project and has salary of ~3f euros ~n', [X, T, M]).
 
  
-
-
-
-
-
-
-
-
